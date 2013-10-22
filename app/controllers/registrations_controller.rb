ecba175a47bfa70bc181b7a6cbfa5be9aa6265c3
Scott C. Reynolds <scott@scottcreynolds.com>
2013-10-22 15:56:44 -0400

    initial commit

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,28 @@
+class RegistrationsController < ApplicationController
+
+  get '/register' do
+    erb :register
+  end
+
+  get '/new' do
+    # You will need to send properly registered people to me
+    # and render a template that will tell them they
+    # have completed their quest
+
+    # keep this line of code in place to protect this sacred
+    # page from interlopers who have not properly completed
+    # the maze you are constructing. You will need to implement
+    # the code to allow them to pass this challenge when
+    # they have registered
+    throw Unauthorized unless user_registered?
+    # Bonus experience! This makes an ugly error page happen.
+    # Can you check for this same condition, but send them back to
+    # the registration page with an error message that will tell
+    # them what they've done wrong?
+  end
+
+  # You'll need a way to send the registration form here.
+  # Use your battle-hardened text editor to create the
+  # appropriate route, look at the request, and then
+  # direct them to the new user page via the route above.
+end
